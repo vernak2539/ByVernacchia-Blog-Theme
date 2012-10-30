ff91a8698a273bae5565e1067c2e536b6ce00652
Alex Vernacchia <alvernacchia@gmail.com>
2012-10-29 23:05:05 -0400

    Added link to github code

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
   <div class="container">
     <div class="row-fluid copyright">
       <div class="span12">
-        &copy; 2010-<?php echo date('Y'); ?> Alex Vernacchia. Theme <a href="#"><!-- GITHUB link to theme code -->ByVernacchia</a> by Alex Vernacchia
+        &copy; 2010-<?php echo date('Y'); ?> Alex Vernacchia. Theme <a href="https://github.com/vernak2539/ByVernacchia-Blog-Theme" target="_blank">ByVernacchia</a> by Alex Vernacchia
       </div>
     </div>
   </div>
