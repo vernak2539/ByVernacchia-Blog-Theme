c222432a1acc9016e2b62f59732b57b134fd0e60
Alex Vernacchia <alvernacchia@gmail.com>
2012-10-29 22:49:57 -0400

    Updated for use with theme

--- 404.php
+++ 404.php
@@ -1,25 +1,15 @@
 <?php
-get_header();
-?>
-		<style type="text/css">
-			#right p {
-				margin:0 0 1px 0;
-			}
-			#right #toDo, #right #toDo li {
-				list-style:disc;
-				padding:3px 0;
-			}
-			#toDo {
-				margin:0 0 0 20px;
-			}
-		</style>
 
-        	<h3>Oooops...404</h3>
-			<p style="font-size:14px;color:#999;font-style:italic">Now that's not right. This page, <?php echo $_SERVER['REQUEST_URI']; ?>, doesn't exist and probably never has. If you feel you've reached it in error, please <a href="/contact">contact me</a> about it.</p>
-			<div style="height:359px; background:url(/images/404.png) no-repeat;"></div>
-            <p style="font-size:14px;color:#999;font-style:italic">And stop breaking my site. Look at what you have done.</p>
-        </div>
-        <div id="right">
-			<?php get_sidebar(); ?> 
-        </div>
-<?php get_footer(); ?>
+get_header();
+
+?>
+
+  <h3 style="color:#666;">Opps...</h3>
+  <p><strong>Looks like this page <em style="color:#888">(<?php echo $_SERVER['REQUEST_URI']; ?>)</em> doesn't exist. Look what you found though.</strong></p>
+  <div class="video-container">  
+    <iframe width="620" height="465" src="http://www.youtube.com/embed/oHg5SJYRHA0?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+  </div>
+  <p>I had to, sorry!</p>
+        
+<?php
+get_footer();
