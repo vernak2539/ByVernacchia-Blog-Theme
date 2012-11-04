beb7a942880451896212ca8b0908e23f9162d53f
Alex Vernacchia <alvernacchia@gmail.com>
2012-11-03 22:02:24 -0400

    Updated single.php title

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
-    <title><?php bloginfo('name'); ?><?php if(is_single()) { ?> | the_title("", "", true); <?php } ?></title>
+    <title><?php bloginfo('name'); ?><?php if(is_single()) { echo ' | ' .  the_title("", "", false); } ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="Alex Vernacchia">
