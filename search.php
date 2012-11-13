edeca25797cce0dfbdaf305b3e31f6f6427db4fb
Alex Vernacchia <alvernacchia@gmail.com>
2012-11-12 20:49:35 -0500

    updated to use index template for post display

--- search.php
+++ search.php
@@ -20,29 +20,7 @@ get_header();
   if(have_posts()) { 
     while(have_posts()) { 
       the_post();
-  ?>
-    <div id="post-<?php the_ID(); ?>" class="post">
-      <div class="post-date">
-        <span><?php the_date('m.d.Y'); ?></span>
-      </div>
-      <?php edit_post_link('Edit', '<div class="edit-link">', '</div>'); ?>
-      <h3><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
-      <div class="entry">
-        <?php the_excerpt(); ?>
-        <div class="postmetadata">
-          <div><?php _e('Posted In: '); ?><?php the_category(', ') ?></div>
-          <div class="post-tagged"> 
-          <?php if(get_the_tag_list()) { ?>
-            <?php echo get_the_tag_list('Tagged: ',', ',''); ?> 
-          <?php } ?>
-          </div>
-          <div class="post-comments">
-            <?php comments_popup_link('<img src="'.get_template_directory_uri().'/img/comment_add.png" width="28" height="28" />', '<div class="multiple-comments">1</div>', '<div class="multiple-comments">%</div>'); ?>
-          </div>
-        </div>
-      </div>
-    </div>
-  <?php
+      get_template_part('content', 'index');
     } // end while
   ?>
     <div class="page-navigation pagination">
