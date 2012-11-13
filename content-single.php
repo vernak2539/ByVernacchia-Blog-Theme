23b2c9e7256054e1303cace43102be57a695e2ba
Alex Vernacchia <alvernacchia@gmail.com>
2012-11-13 17:52:09 -0500

    changed date display

--- content-single.php
+++ content-single.php
@@ -3,12 +3,25 @@
 // used in loop, well not really loop
 ?>
 <article id="post-<?php the_ID(); ?>" class="post-full">
+  <div class="post-date visible-desktop">
+    <div class="post-date-circle">
+      <div class="day">
+        <?php echo get_the_date('j'); ?>
+      </div>
+      <div class="month">
+        <?php echo get_the_date('M'); ?>
+      </div>
+    </div>
+    <div class="year">
+      <?php echo get_the_date('Y'); ?>
+    </div>
+  </div>
   <h2>
     <?php the_title(); ?>
     <?php edit_post_link('Edit', '<span class="label label-info edit-content-btn">', '</span>'); ?>
   </h2>
 
-  <div class="post-meta">
+  <div class="post-meta hidden-desktop">
     <?php
     printf( __( 'Posted on <a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="byline"> by <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>'),
         esc_url( get_permalink() ),
